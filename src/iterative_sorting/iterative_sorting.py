36ff61e8a7cfcf883d78672cd93a8c7b0b4f79fb
shenry45 <shawn45henry@gmail.com>
2020-01-29 22:13:51 -0700

    count sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,3 +1,5 @@
+import re
+
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
@@ -26,12 +28,12 @@ def bubble_sort( arr ):
     while (element_swap_in_pass == True):
         swap_counter = 0
 
-        # loop through arr
         for i in range(0, len(arr) - 1):
             if (arr[i] > arr[i + 1]):
                 value_holder = arr[i]
                 arr[i] = arr[i + 1]
                 arr[i + 1] = value_holder
+
                 swap_counter = swap_counter + 1
 
         if (swap_counter == 0):
@@ -43,4 +45,43 @@ def bubble_sort( arr ):
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
 
-    return arr
+    def array_has_zero_negative_numbers():
+        for i in range(0, len(arr)):
+            search = re.search('^\-', f'{arr[i]}')
+
+            if (search is not None):
+                return False
+
+        return True
+
+
+    if len(arr) > 0 and array_has_zero_negative_numbers():
+        def find_highest_arr_numb():
+            if (max(arr) > len(arr)):
+                return max(arr)
+            else:
+                return len(arr)
+
+        count_tracker = [0 for i in range(0, find_highest_arr_numb() + 1)]
+
+        for i in range(0, len(arr)):
+            current_value = arr[i]
+            count_tracker[current_value] += 1
+
+        sorted_arr = []
+
+        for i in range(0, len(count_tracker)):
+            multiples_arr = []
+            
+            for j in range(0, count_tracker[i]):
+                multiples_arr.append(i)
+
+            sorted_arr += multiples_arr
+            
+        print(sorted_arr)
+
+        return sorted_arr
+    elif array_has_zero_negative_numbers() == False:
+        return 'Error, negative numbers not allowed in Count Sort'
+    else:
+        return []

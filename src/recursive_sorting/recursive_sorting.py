477fdd75ceba13ba46d43d08c99c14ddd4102467
shenry45 <shawn45henry@gmail.com>
2020-01-31 20:21:28 -0700

    merge sort progress, incomplete, splits into arrays of 2 and sorts those but no rejoining sort implimented

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,17 +1,45 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
+import math
+
+# TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
-    # TO-DO
-    
-    return merged_arr
+    index = 0
 
+    for el in arrA:
+        merged_arr[index] = el
+        index += 1
+
+    for el in arrB:
+        merged_arr[index] = el
+        index += 1
+
+    return merged_arr
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
-    # TO-DO
+    print(arr, 'iteration')
+
+    if len(arr) == 1:
+        return arr
+
+    elif len(arr) == 2:
+        if arr[0] > arr[1]:
+            holder = arr[0]
+            arr[0] = arr[1]
+            arr[1] = holder
+        
+        print(arr, '2 values')
+        return arr
+
+    else:
+        lhs = arr[:math.floor(len(arr)/2)]
+        rhs = arr[math.floor(len(arr)/2):]
+        return merge_sort(lhs) + merge_sort(rhs)       
 
     return arr
+
+print(merge_sort([5, 1, 4, 2, 9, 6, 0, 7, 3]))
 
 
 # STRETCH: implement an in-place merge sort algorithm
